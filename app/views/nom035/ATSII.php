42e2b448d95308980d6720073edac767b02c3810
Manuel Ramirez Coronel <ra_cm@outlook.com>
2019-12-13 16:06:38 -0600

    Update ATSII.php
    Add bootstrap and change the radio buttons for inline

--- app/views/nom035/ATSII.php
+++ app/views/nom035/ATSII.php
@@ -1,87 +1,288 @@
-<p>
-	Tenga presente que el siguiete cuestionario se debe de responder únicamente si respondió que sí a algún reactivo del primer cuestionario para identificar a los trabajadores que fueron sujetos a acontecimientos traumáticos severos. 
-</p>
-<form method="POST" action="<?php echo $this->_config->get('app/webbase'); ?>RegistrarATSII/">
-	<h3>II.- Recuerdos persistentes sobre el acontecimiento (durante el último mes):</h3>
-
-	<label>¿Ha tenido recuerdos recurrentes sobre el acontecimiento que le provocan malestares?</label>
-		<p>
-		<input type="radio" value="1" name="ATS_RECUERDOS" required>Sí <br>
-		<input type="radio" value="2" name="ATS_RECUERDOS" required checked>No <br>
-		</p>
-	<label>¿Ha tenido sueños de carácter recurrente sobre el acontecimiento, que le producen malestar?</label>
-		<p>
-		<input type="radio" value="1" name="ATS_SUENOS" required>Sí <br>
-		<input type="radio" value="2" name="ATS_SUENOS" required checked>No <br>
-		</p>
-	<h3>III.- Esfuerzo por evitar circunstancias parecidas o asociadas al acontecimiento (durante el último mes):</h3>
-
-	<label>¿Se ha esforzado por evitar todo tipo de sentimientos, conversaciones o situaciones que le puedan recordar el acontecimiento?</label>
-		<p>
-		<input type="radio" value="1" name="ATS_SENTIMIENTOS" required>Sí <br>
-		<input type="radio" value="2" name="ATS_SENTIMIENTOS" required checked>No <br>
-		</p>
-	<label>¿Se ha esforzado por evitar todo tipo de actividades, lugares o personas que motivan recuerdos del acontecimiento?</label>
-		<p>
-		<input type="radio" value="1" name="ATS_ACTIVIDADES" required>Sí <br>
-		<input type="radio" value="2" name="ATS_ACTIVIDADES" required checked>No <br>
-		</p> 
-	<label>¿Ha tenido dificultad para recordar alguna parte importante del evento?</label>
-		<p>
-		<input type="radio" value="1" name="ATS_DIFICULTAD" required>Sí <br>
-		<input type="radio" value="2" name="ATS_DIFICULTAD" required checked>No <br>
-		</p>
-	 
-	<label>¿Ha disminuido su interés en sus actividades cotidianas?</label>
-		<p>
-		<input type="radio" value="1" name="ATS_INTERES" required>Sí <br>
-		<input type="radio" value="2" name="ATS_INTERES" required checked>No <br>
-		</p>
-	 
-	<label>¿Se ha sentido usted alejado o distante de los demás?</label>
-		<p>
-		<input type="radio" value="1" name="ATS_ALEJADO" required>Sí <br>
-		<input type="radio" value="2" name="ATS_ALEJADO" required checked>No <br>
-		</p>
-	<label>¿Ha notado que tiene dificultad para expresar sus sentimientos?</label>
-		<p>
-		<input type="radio" value="1" name="ATS_EXPRESAR" required>Sí <br>
-		<input type="radio" value="2" name="ATS_EXPRESAR" required checked>No <br>
-		</p> 
-	<label>¿Ha tenido la impresión de que su vida se va a acortar, que va a morir antes que otras personas o que tiene un futuro limitado?</label>
-		<p>
-		<input type="radio" value="1" name="ATS_IMPRESION" required>Sí <br>
-		<input type="radio" value="2" name="ATS_IMPRESION" required checked>No <br>
-		</p>
-		<h3>IV Afectación (durante el último mes):</h3>
-
-	<label>¿Ha tenido usted dificultades para dormir?</label>
-		<p>
-		<input type="radio" value="1" name="ATS_DORMIR" required>Sí <br>
-		<input type="radio" value="2" name="ATS_DORMIR" required checked>No <br>
-		</p> 
-	<label>¿Ha estado particularmente irritable o le han dado arranques de coraje?</label>
-		<p>
-		<input type="radio" value="1" name="ATS_IRRITABLE" required>Sí <br>
-		<input type="radio" value="2" name="ATS_IRRITABLE" required checked>No <br>
-		</p>
-	<label>¿Ha tenido dificultad para concentrarse?</label>
-		<p>
-		<input type="radio" value="1" name="ATS_CONCENTRARSE" required>Sí <br>
-		<input type="radio" value="2" name="ATS_CONCENTRARSE" required checked>No <br>
-		</p>
-	<label>¿Ha estado nervioso o constantemente en alerta?</label>
-		<p>
-		<input type="radio" value="1" name="ATS_NERVIOSO" required>Sí <br>
-		<input type="radio" value="2" name="ATS_NERVIOSO" required checked>No <br>
-		</p> 
-	<label>¿Se ha sobresaltado fácilmente por cualquier cosa?</label>
-		<p>
-		<input type="radio" value="1" name="ATS_SOBRESALTO" required>Sí <br>
-		<input type="radio" value="2" name="ATS_SOBRESALTO" required checked>No <br>
-		</p>
-	<p>
-			<input type="hidden" name="token" value="<?php echo $this->_token->generate(); ?>" required>
-			<input type="submit">
-		</p>
-</form>	
+<br>
+<div class="section">
+	<div class="alert alert-warning" role="alert">
+		<div class="row">
+			<div class="col-2 text-center">
+				<i class="fa fa-exclamation-circle fa-3x animated bounceIn slow"></i>
+			</div>
+			<div class="col">
+				Tenga presente que el siguiente cuestionario se debe de responder únicamente si respondió que sí a algún
+				reactivo del
+				primer cuestionario para identificar a los trabajadores que fueron sujetos a acontecimientos traumáticos
+				severos.
+			</div>
+		</div>
+	</div>
+	<form method="POST" action="<?php echo $this->_config->get('app/webbase'); ?>RegistrarATSII/">
+		<div class="card">
+			<h5 class="card-header">II.- Recuerdos persistentes sobre el acontecimiento (durante el último mes):
+			</h5>
+			<div class="card-body">
+				<table class="table table-hover">
+					<tbody>
+						<tr>
+							<td>¿Ha tenido recuerdos recurrentes sobre el acontecimiento que le provocan malestares?
+							</td>
+							<td>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_RECUERDOS" id="ATS_RECUERDOS"
+										value="1" required>
+									<label class="form-check-label" for="ATS_RECUERDOS">Sí</label>
+								</div>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_RECUERDOS" id="ATS_RECUERDOS"
+										value="2" required checked>
+									<label class="form-check-label" for="ATS_RECUERDOS">No</label>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td>¿Ha tenido sueños de carácter recurrente sobre el acontecimiento, que le producen
+								malestar?
+							</td>
+							<td>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_SUENOS" id="ATS_SUENOS"
+										value="1" required>
+									<label class="form-check-label" for="ATS_SUENOS">Sí</label>
+								</div>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_SUENOS" id="ATS_SUENOS"
+										value="2" required checked>
+									<label class="form-check-label" for="ATS_SUENOS">No</label>
+								</div>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<br>
+		<div class="card">
+			<h5 class="card-header">III.- Esfuerzo por evitar circunstancias parecidas o asociadas al acontecimiento
+				(durante el último mes):</h5>
+			<div class="card-body">
+				<table class="table table-hover">
+					<tbody>
+						<tr>
+							<td>
+								¿Se ha esforzado por evitar todo tipo de sentimientos, conversaciones o situaciones
+								que le
+								puedan recordar el
+								acontecimiento?
+							</td>
+							<td>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_SENTIMIENTOS"
+										id="ATS_SENTIMIENTOS" value="1" required>
+									<label class="form-check-label" for="ATS_SENTIMIENTOS">Sí</label>
+								</div>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_SENTIMIENTOS"
+										id="ATS_SENTIMIENTOS" value="2" required checked>
+									<label class="form-check-label" for="ATS_SENTIMIENTOS">No</label>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								¿Se ha esforzado por evitar todo tipo de actividades, lugares o personas que motivan
+								recuerdos del
+								acontecimiento?
+							</td>
+							<td>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_ACTIVIDADES"
+										id="ATS_ACTIVIDADES" value="1" required>
+									<label class="form-check-label" for="ATS_ACTIVIDADES">Sí</label>
+								</div>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_ACTIVIDADES"
+										id="ATS_ACTIVIDADES" value="2" required checked>
+									<label class="form-check-label" for="ATS_ACTIVIDADES">No</label>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								¿Ha tenido dificultad para recordar alguna parte importante del evento?
+							</td>
+							<td>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_DIFICULTAD"
+										id="ATS_DIFICULTAD" value="1" required>
+									<label class="form-check-label" for="ATS_DIFICULTAD">Sí</label>
+								</div>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_DIFICULTAD"
+										id="ATS_DIFICULTAD" value="2" required checked>
+									<label class="form-check-label" for="ATS_DIFICULTAD">No</label>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								¿Ha disminuido su interés en sus actividades cotidianas?
+							</td>
+							<td>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_INTERES" id="ATS_INTERES"
+										value="1" required>
+									<label class="form-check-label" for="ATS_INTERES">Sí</label>
+								</div>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_INTERES" id="ATS_INTERES"
+										value="2" required checked>
+									<label class="form-check-label" for="ATS_INTERES">No</label>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								¿Se ha sentido usted alejado o distante de los demás?
+							</td>
+							<td>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_ALEJADO" id="ATS_ALEJADO"
+										value="1" required>
+									<label class="form-check-label" for="ATS_ALEJADO">Sí</label>
+								</div>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_ALEJADO" id="ATS_ALEJADO"
+										value="2" required checked>
+									<label class="form-check-label" for="ATS_ALEJADO">No</label>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								¿Ha notado que tiene dificultad para expresar sus sentimientos?
+							</td>
+							<td>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_EXPRESAR" id="ATS_EXPRESAR"
+										value="1" required>
+									<label class="form-check-label" for="ATS_EXPRESAR">Sí</label>
+								</div>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_EXPRESAR" id="ATS_EXPRESAR"
+										value="2" required checked>
+									<label class="form-check-label" for="ATS_EXPRESAR">No</label>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								¿Ha tenido la impresión de que su vida se va a acortar, que va a morir antes que
+								otras
+								personas o que tiene
+								un
+								futuro limitado?
+							</td>
+							<td>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_IMPRESION" id="ATS_IMPRESION"
+										value="1" required>
+									<label class="form-check-label" for="ATS_IMPRESION">Sí</label>
+								</div>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_IMPRESION" id="ATS_IMPRESION"
+										value="2" required checked>
+									<label class="form-check-label" for="ATS_IMPRESION">No</label>
+								</div>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<br>
+		<div class="card">
+			<h5 class="card-header">IV Afectación (durante el último mes):</h5>
+			<div class="card-body">
+				<table class="table table-hover">
+					<tbody>
+						<tr>
+							<td>¿Ha tenido usted dificultades para dormir?</td>
+							<td>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_DORMIR" id="ATS_DORMIR"
+										value="1" required>
+									<label class="form-check-label" for="ATS_DORMIR">Sí</label>
+								</div>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_DORMIR" id="ATS_DORMIR"
+										value="2" required checked>
+									<label class="form-check-label" for="ATS_DORMIR">No</label>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td>¿Ha estado particularmente irritable o le han dado arranques de coraje?</td>
+							<td>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_IRRITABLE" id="ATS_IRRITABLE"
+										value="1" required>
+									<label class="form-check-label" for="ATS_IRRITABLE">Sí</label>
+								</div>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_IRRITABLE" id="ATS_IRRITABLE"
+										value="2" required checked>
+									<label class="form-check-label" for="ATS_IRRITABLE">No</label>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td>¿Ha tenido dificultad para concentrarse?</td>
+							<td>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_CONCENTRARSE"
+										id="ATS_CONCENTRARSE" value="1" required>
+									<label class="form-check-label" for="ATS_CONCENTRARSE">Sí</label>
+								</div>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_CONCENTRARSE"
+										id="ATS_CONCENTRARSE" value="2" required checked>
+									<label class="form-check-label" for="ATS_CONCENTRARSE">No</label>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td>¿Ha estado nervioso o constantemente en alerta?</td>
+							<td>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_NERVIOSO" id="ATS_NERVIOSO"
+										value="1" required>
+									<label class="form-check-label" for="ATS_NERVIOSO">Sí</label>
+								</div>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_NERVIOSO" id="ATS_NERVIOSO"
+										value="2" required checked>
+									<label class="form-check-label" for="ATS_NERVIOSO">No</label>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td>¿Se ha sobresaltado fácilmente por cualquier cosa?</td>
+							<td>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_SOBRESALTO"
+										id="ATS_SOBRESALTO" value="1" required>
+									<label class="form-check-label" for="ATS_SOBRESALTO">Sí</label>
+								</div>
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="ATS_SOBRESALTO"
+										id="ATS_SOBRESALTO" value="2" required checked>
+									<label class="form-check-label" for="ATS_SOBRESALTO">No</label>
+								</div>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<input type="hidden" name="token" value="<?php echo $this->_token->generate(); ?>" required>
+				<input type="submit" class="btn btn-success btn-block">
+			</div>
+		</div>
+	</form>
+</div>
+<br>
