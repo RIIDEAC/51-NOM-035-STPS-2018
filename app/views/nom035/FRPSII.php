5c8b55293af2d204e15d8e1c8004792531224802
Manuel Ramirez Coronel <ra_cm@outlook.com>
2019-12-31 11:15:53 -0600

    Update FRPSII.php
    Add bootstrap and styles for the table

--- app/views/nom035/FRPSII.php
+++ app/views/nom035/FRPSII.php
@@ -1,50 +1,114 @@
-<form method="post" action="<?php echo $this->_config->get('app/webbase'); ?>RegistrarFRPSII/">
-	<label>En mi trabajo debo brindar servicio a clientes o usuarios:</label><br>
-	<p>
-		<input type="radio" value="1" name="FRPSII" required>Sí <br>
-		<input type="radio" value="2" name="FRPSII" required checked>No <br>
-	</p>
-	<p>
-		En caso de que su respuesta haya sido "Sí" responda los siguientes reactivos, de lo contrario de clic en "Enviar".
-	</p>
-
-	<hr>
-		<h3>Las preguntas siguientes están relacionadas con la atención a clientes y usuarios.</h3>
-		<label for="RPS_L_65">65.- Atiendo clientes o usuarios muy enojados</label><br>
-	<p>
-		<input type="radio" value="4" name="RPS_L_65" required>Siempre <br>
-		<input type="radio" value="3" name="RPS_L_65" required>Casi siempre <br>
-		<input type="radio" value="2" name="RPS_L_65" required>Algunas veces <br>
-		<input type="radio" value="1" name="RPS_L_65" required>Casi nunca <br>
-		<input type="radio" value="0" name="RPS_L_65" required checked>Nunca <br>
-	</p>
-	<label for="RPS_L_66">66.- Mi trabajo me exige atender personas muy necesitadas de ayuda o enfermas</label><br>
-	<p>
-		<input type="radio" value="4" name="RPS_L_66" required>Siempre <br>
-		<input type="radio" value="3" name="RPS_L_66" required>Casi siempre <br>
-		<input type="radio" value="2" name="RPS_L_66" required>Algunas veces <br>
-		<input type="radio" value="1" name="RPS_L_66" required>Casi nunca <br>
-		<input type="radio" value="0" name="RPS_L_66" required checked>Nunca <br>
-	</p>
-	<label for="RPS_L_67">67.- Para hacer mi trabajo debo demostrar sentimientos distintos a los míos</label><br>
-	<p>
-		<input type="radio" value="4" name="RPS_L_67" required>Siempre <br>
-		<input type="radio" value="3" name="RPS_L_67" required>Casi siempre <br>
-		<input type="radio" value="2" name="RPS_L_67" required>Algunas veces <br>
-		<input type="radio" value="1" name="RPS_L_67" required>Casi nunca <br>
-		<input type="radio" value="0" name="RPS_L_67" required checked>Nunca <br>
-	</p>
-	<label for="RPS_L_68">68.- Mi trabajo me exige atender situaciones de violencia</label><br>
-	<p>
-		<input type="radio" value="4" name="RPS_L_68" required>Siempre <br>
-		<input type="radio" value="3" name="RPS_L_68" required>Casi siempre <br>
-		<input type="radio" value="2" name="RPS_L_68" required>Algunas veces <br>
-		<input type="radio" value="1" name="RPS_L_68" required>Casi nunca <br>
-		<input type="radio" value="0" name="RPS_L_68" required checked>Nunca <br>
-	</p>
-	<hr>
-	<p>
-		<input type="hidden" name="token" value="<?php echo $this->_token->generate(); ?>">
-		<input type="submit">
-	</p>
-</form>
+<br>
+<div class="card">
+	<div class="card-header">
+		FRPSII
+	</div>
+	<div class="card-body">
+		<label for="FRPSII">En mi trabajo debo brindar servicio a clientes o usuarios: </label>
+		<div class="form-check form-check-inline">
+			<input class="form-check-input" type="radio" value="1" name="FRPSII" required>Sí
+		</div>
+		<div class="form-check form-check-inline">
+			<input class="form-check-input" type="radio" value="2" name="FRPSII" required checked>No <br>
+		</div>
+		<div class="alert alert-info">
+			En caso de que su respuesta haya sido "Sí" responda los siguientes reactivos, de lo contrario de clic en
+			"Enviar".
+		</div>
+		<form method="post" action="<?php echo $this->_config->get('app/webbase'); ?>RegistrarFRPSII/">
+			<hr>
+			<div class="alert alert-warning">
+				Las preguntas siguientes están relacionadas con la atención a clientes y usuarios.
+			</div>
+			<div class="table-responsive-sm">
+				<table class="table">
+					<tr>
+						<th></th>
+						<th class="tb-header" bgcolor="#d32f2f">Siempre</th>
+						<th class="tb-header" bgcolor="#ef6c00">Casi siempre</th>
+						<th class="tb-header" bgcolor="#fbc02d">Algunas veces </th>
+						<th class="tb-header" bgcolor="#388e3c">Casi nunca</th>
+						<th class="tb-header" bgcolor="#1976d2">Nunca</th>
+					</tr>
+					<tr>
+						<td>65.- Atiendo clientes o usuarios muy enojados</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="4" name="RPS_L_65" required>
+						</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="3" name="RPS_L_65" required>
+						</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="2" name="RPS_L_65" required>
+						</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="1" name="RPS_L_65" required>
+						</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="0" name="RPS_L_65" required checked>
+						</td>
+					</tr>
+					<tr>
+						<td>66.- Mi trabajo me exige atender personas muy necesitadas de ayuda o enfermas</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="4" name="RPS_L_66" required>
+						</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="3" name="RPS_L_66" required>
+						</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="2" name="RPS_L_66" required>
+						</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="1" name="RPS_L_66" required>
+						</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="0" name="RPS_L_66" required checked>
+						</td>
+					</tr>
+					<tr>
+						<td>67.- Para hacer mi trabajo debo demostrar sentimientos distintos a los míos</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="4" name="RPS_L_67" required>
+						</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="3" name="RPS_L_67" required>
+						</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="2" name="RPS_L_67" required>
+						</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="1" name="RPS_L_67" required>
+						</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="0" name="RPS_L_67" required checked>
+						</td>
+					</tr>
+					<tr>
+						<td>68.- Mi trabajo me exige atender situaciones de violencia</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="4" name="RPS_L_68" required>
+						</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="3" name="RPS_L_68" required>
+						</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="2" name="RPS_L_68" required>
+						</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="1" name="RPS_L_68" required>
+						</td>
+						<td class="align-middle rd-center">
+							<input type="radio" value="0" name="RPS_L_68" required checked>
+						</td>
+					</tr>
+				</table>
+			</div>
+			<p>
+				<input type="hidden" name="token" value="<?php echo $this->_token->generate(); ?>">
+				<input class="btn btn-block btn-success" type="submit">
+			</p>
+		</form>
+	</div>
+</div>
+<br>
